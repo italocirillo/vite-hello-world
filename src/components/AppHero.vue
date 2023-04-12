ce19012b4b1a49a3c14599a336f977287cc89ee4
italocirillo <italocirillo@gmail.com>
2023-04-12 16:27:51 +0200

    ESERCIZIO COMPLETO + BONUS

---
+++ src/components/AppHero.vue
@@ -0,0 +1,36 @@
+<script>
+export default {
+    name: "AppHero",
+    data() {
+        return {
+            title: "Jumbotron",
+        }
+    }
+
+}
+</script>
+
+
+<template>
+    <div class="jumbotron">
+        <div class="header-jumbotron">
+            <h4>{{ title }}</h4>
+            <img src="../assets/vue.svg" alt="logo Vue">
+        </div>
+        <img src="../assets/img/jumbotron.png" alt="Immagine banner">
+    </div>
+</template>
+
+
+<style>
+.jumbotron {
+    padding: 1em;
+    border: 2px solid white;
+}
+
+.header-jumbotron {
+    text-transform: uppercase;
+    display: flex;
+    justify-content: space-between;
+}
+</style>
